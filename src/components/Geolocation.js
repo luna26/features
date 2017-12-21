65210b5f9ea9dfe7705d716e92ffbe65d578be34
luna26 <33940028+luna26@users.noreply.github.com>
2017-12-21 14:28:06 -0600

    geolocation finished

--- src/components/Geolocation.js
+++ src/components/Geolocation.js
@@ -1,44 +1,65 @@
 import React, { Component } from 'react';
-import {View, Text} from 'react-native';
+import { View, Text, Button } from 'react-native';
+import openMap from 'react-native-open-maps';
 
-class GeolocationComp extends Component{
+class GeolocationComp extends Component {
     constructor(props) {
         super(props);
-    
+
         this.state = {
-          latitude: null,
-          longitude: null,
-          error: null,
+            latitude: null,
+            longitude: null,
+            error: null,
         };
-      }
-    
-      componentDidMount() {
-        console.log(this.state);
+    }
+
+    componentDidMount() {
         navigator.geolocation.watchPosition((position) => {
-            console.log(position);
             this.setState({
                 latitude: position.coords.latitude,
                 longitude: position.coords.longitude
             });
             console.log(this.state);
         });
-      }
-    
-      componentWillUnmount() {
+    }
+
+    componentWillUnmount() {
         navigator.geolocation.clearWatch(this.watchId);
-      }
-    
-      render() {
+    }
+
+    _goToYosemite(lat, long) {
+        openMap({ latitude: this.state.latitude, longitude: this.state.longitude });
+    }
+
+    renderButton() {
+        if(this.state.latitude != null && this.state.latitude != undefined){
+            return (
+                <Button
+                    color={'#bdc3c7'}
+                    onPress={this._goToYosemite.bind(this)}
+                    title="Click To Open Maps 🗺" />
+            );
+        }
+    }
+
+    setInfo() {
+        lat = this.state.latitude;
+        long = this.state.longitude;
+        this._goToYosemite(lat, long);
+    }
+
+    render() {
         return (
-          <View style={{ flexGrow: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text>Latitude: {this.state.latitude}</Text>
-            <Text>Longitude: {this.state.longitude}</Text>
-            {this.state.error ? <Text>Error: {this.state.error}</Text> : null}
-          </View>
+            <View style={{ flexGrow: 1, alignItems: 'center', justifyContent: 'center' }}>
+                <Text>Latitude: {this.state.latitude}</Text>
+                <Text>Longitude: {this.state.longitude}</Text>
+                {this.state.error ? <Text>Error: {this.state.error}</Text> : null}
+                {this.renderButton()}
+            </View>
         );
-      }
-    
-    
+    }
+
+
 }
 
 export default GeolocationComp;
